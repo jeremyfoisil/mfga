a24a70ad44375fda7868482fd1fcdce9d9ecaa2c
Jérémy F. <jeremy.foisil@kermap.com>
2026-05-22 09:42:13 +0200

    feat: add TypeScript types

---
+++ src/types/index.ts
@@ -0,0 +1,82 @@
+export interface Goal {
+  name: string
+  minute: number
+  penalty?: boolean
+  owngoal?: boolean
+}
+
+export interface MatchResult {
+  home: string
+  away: string
+  goalsHome: Goal[]
+  goalsAway: Goal[]
+  goalsHomeText: string
+  goalsAwayText: string
+}
+
+export interface Match {
+  id: number
+  stage: string
+  group: string | null
+  home: string
+  away: string
+  homeKnown: boolean
+  awayKnown: boolean
+  homeLabel: string
+  awayLabel: string
+  result: MatchResult
+  matchDate: string
+  matchTime: string
+  venue: string
+  round: string
+}
+
+export interface Participant {
+  id: number
+  name: string
+  color: string
+}
+
+export interface Prono {
+  home: string
+  away: string
+}
+
+export interface BonusType {
+  id: string
+  label: string
+  points: number
+  count: number
+}
+
+export interface BonusIcon {
+  icon: string
+  bg: string
+}
+
+export interface ConfettiBit {
+  left: string
+  delay: string
+  dur: string
+  color: string
+}
+
+export interface MeltingPotFlag {
+  c: string
+  x: string
+  y: string
+  w: number
+  h: number
+  r: number
+}
+
+export interface KOStage {
+  id: string
+  label: string
+}
+
+export interface RankedParticipant extends Participant {
+  total: number
+  exactCount: number
+  diagCount: number
+}
